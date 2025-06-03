bb6ae24ee4e7c297024125d499e1c193a72e33c1
im-faix <faexan92@gmail.com>
2025-06-03 07:16:31 +0530

    updated index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+// pages/index.jsx
 import React from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
@@ -5,15 +6,19 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Certifications from "../components/Certifications";
 import Contact from "../components/Contact";
+import Navbar from "../components/Navbar";
+import Resume from "../components/Resume";
 
 export default function Home() {
   return (
     <main className="bg-gray-900 text-white min-h-screen px-6 py-8">
+      <Navbar />
       <Hero />
       <About />
       <Skills />
       <Projects />
       <Certifications />
+      <Resume />
       <Contact />
     </main>
   );
