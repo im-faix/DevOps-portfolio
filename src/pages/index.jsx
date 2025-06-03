28e18fef469b8ef28a924f5e0dae97e8e8a583f0
im-faix <faexan92@gmail.com>
2025-06-03 07:27:23 +0530

    updated index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,18 +8,21 @@ import Certifications from "../components/Certifications";
 import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
 import Resume from "../components/Resume";
+import ScrollRevealWrapper from "../components/ScrollRevealWrapper";
 
 export default function Home() {
   return (
     <main className="bg-gray-900 text-white min-h-screen px-6 py-8">
       <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Certifications />
-      <Resume />
-      <Contact />
+      <ScrollRevealWrapper>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Certifications />
+        <Resume />
+        <Contact />
+      </ScrollRevealWrapper>
     </main>
   );
 }
